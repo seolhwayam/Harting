761a44956e0c282863294fad269e5693fc5c7964
kcm <kcm5450>
2024-04-24 09:14:06 +0900

    ddd

---
+++ src/assets/quiz.js
@@ -0,0 +1,15 @@
+
+
+// JS LV 0 - 1번 변수를 선언하는 올바른 방법은?
+(int value;, variable value;, let value;, dim value;)
+
+// JS LV 0 - 2번 만약 변수 x의 값이 4라고 가정하자. "document.write(x++);" 문장이 실행된다면 브라우저에 표기되는 값은 얼마인가?
+(4, 5, 3, undefined)
+
+// JS LV 1 - 1번 객체를 생성하는 키워드는?
+(instance, object, new, create)
+
+// JS LV 1 - 2번 객체에 존재하지 않은 속성을 꺼내려고 하면 어떤 값이 반환되는가?
+(null, undefined, -1, 0)
+
+// JS LV 2 -
